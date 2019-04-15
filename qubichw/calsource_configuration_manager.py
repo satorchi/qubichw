6752e881b973820dc2fcdfb7449e01a42382b5c0
satorchi <satorchi@apc.in2p3.fr>
2019-04-15 16:25:30 +0200

    status returns strings now

--- qubichw/calsource_configuration_manager.py
+++ qubichw/calsource_configuration_manager.py
@@ -352,7 +352,17 @@ class calsource_configuration_manager():
             if settings is None:
                 msg += ' | %s: FAILED TO READ SETTINGS' % dev
             else:
+                '''
+                # for the HP3312A
                 msg += '| %s: SHAPE=%s FREQUENCY=%.6f Hz AMPLITUDE=%.6f V OFFSET=%.6f V DUTY CYCLE=%.1f%%' % \
+                    (dev,
+                     settings['shape'],
+                     settings['frequency'],
+                     settings['amplitude'],
+                     settings['offset'],
+                     settings['duty'])
+                '''
+                msg += '| %s: SHAPE=%s FREQUENCY=%s AMPLITUDE=%s OFFSET=%s DUTY CYCLE=%s' % \
                     (dev,
                      settings['shape'],
                      settings['frequency'],
