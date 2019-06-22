dc1d0526da126c43b6de7c9146a99c6977278e1a
satorchi <satorchi@apc.in2p3.fr>
2019-06-22 08:26:12 +0200

    read data from the Raspberry Pi ADC and send it via socket (code by Manuel Gonzalez)

---
+++ qubichw/read_calsource.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+'''
+$Id: read_calsource.py
+$auth: Manuel Gonzalez <manuel.gonzalez@ib.edu.ar>
+$created: Tue 30 Apr 18:16:04 CEST 2019
+$license: GPLv3 or later, see https://www.gnu.org/licenses/gpl-3.0.txt
+
+          This is free software: you are free to change and
+          redistribute it.  There is NO WARRANTY, to the extent
+          permitted by law.
+
+Read data from the ADC on the Raspberry Pi, and send it via socket
+'''
+import board
+import busio
+from digitalio import DigitalInOut, Direction
+from adafruit_ads1x15.analog_in import AnalogIn
+import adafruit_ads1x15.ads1015 as ADS
+import socket
+import datetime as dt
+
+ADC_RATE = 3300
+data_rate = 300.
+deltat = dt.timedelta(microseconds=1e6/data_rate*0.85)
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+ads = ADS.ADS1015(i2c)
+ads.gain = 2
+ads.data_rate = ADC_RATE
+
+chan0 = AnalogIn(ads, ADS.P0)
+
+ads.mode = 0x0000
+ads.comparator_config = 0
+
+#IP_BROADCAST = "192.168.2.255"
+IP_QUBIC_CENTRAL = "192.168.2.1"
+IP_PIGPS = "192.168.2.17"
+IP_QUBICSTUDIO = "192.168.2.8"
+
+receivers = [IP_QUBIC_CENTRAL,
+             IP_PIGPS,
+             IP_QUBICSTUDIO ]
+
+
+PORT = 31337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+date_now = dt.datetime.utcnow()
+old_date = date_now
+old_print_date = date_now
+count = 0
+
+while True:
+    value = chan0.value
+    date_now = dt.datetime.utcnow()
+    if(date_now-old_date>deltat):
+        sdata = date_now.strftime("%s.%f") + "\t" + str(value)
+        for rx in receivers:
+            s.sendto(sdata.encode(), (rx,PORT))
+        old_date = date_now 
+        count+=1
+    if(date_now-old_print_date > dt.timedelta(seconds=1)):
+        string_to_print = sdata + "\tRate:% d" %count
+        print(string_to_print, end='\r',flush=True)
+        old_print_date = date_now
+        count=0
+
+
