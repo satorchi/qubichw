7b14b2e7a3e1253c8789e42b4defe3696d4d0d12
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2019-08-07 15:25:12 +0200

    replace TG5012A weirdness

--- qubichw/modulator_tg5012a.py
+++ qubichw/modulator_tg5012a.py
@@ -166,7 +166,7 @@ class tg5012:
 
         else:
             self.settings['amplitude'] = filter(lambda x: x in string.printable, self.answer[128:156]).replace(' ','')
-            self.settings['frequency'] = filter(lambda x :x in string.printable, self.answer[8:36]).replace('HzHzz','Hz').replace(' ','')
+            self.settings['frequency'] = filter(lambda x :x in string.printable, self.answer[8:36]).replace('HzHzz','Hz').replace(' ','').replace('Hzzz','Hz')
             self.settings['offset'] = filter(lambda x :x in string.printable, self.answer[244:272]).replace(' ','')
             self.settings['duty'] = filter(lambda x :x in string.printable, self.answer[356:384]).replace(' ','')
     
