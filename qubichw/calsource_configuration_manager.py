3117c4274669c27a955922adb71e7bd76ea30000
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2019-11-12 15:11:58 +0100

    bugfix

--- qubichw/calsource_configuration_manager.py
+++ qubichw/calsource_configuration_manager.py
@@ -90,9 +90,9 @@ class calsource_configuration_manager():
         initialize variables, depending on the role
         if the role is "manager", we need to connect to the hardware
         '''
-        self.log('calsource configuration manager 0: assigning variables: role=%s' % role)
-
         self.role = role
+        self.log('calsource configuration manager: assigning variables: role=%s' % role)
+
         self.date_fmt = '%Y-%m-%d %H:%M:%S.%f'
         self.device_list = ['modulator','calsource','lamp','amplifier','arduino']
 
