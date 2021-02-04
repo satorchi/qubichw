76d4dc7987210dd350e1abcee5b894883b0ba3f3
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2021-02-04 19:38:13 +0100

    improved messages for ups alarm

--- scripts/ups_alarm.py
+++ scripts/ups_alarm.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''
-$Id: mailip.py
+$Id: ups_alarm.py
 $auth: Steve Torchinsky <satorchi@apc.in2p3.fr>
 $created: Fri 22 Jan 15:17:12 CET 2021
 $license: GPLv3 or later, see https://www.gnu.org/licenses/gpl-3.0.txt
@@ -18,12 +18,34 @@ from email.mime.text import MIMEText
 
 import sys,os,time,subprocess
 
-cmd='hostname'
-proc=subprocess.Popen(cmd,stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-out,err=proc.communicate()
-hostname=out.decode().strip()
+cmd = 'hostname'
+proc = subprocess.Popen(cmd,stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+out,err = proc.communicate()
+hostname = out.decode().strip()
 
-txtmsg = 'QUBIC is running on battery!'
+cmd = 'upsc cyberpower'
+proc = subprocess.Popen(cmd,stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+out,err = proc.communicate()
+
+msg_list = []
+lines = out.decode().split('\n')
+for line in lines:
+    col = line.split(':')
+    if len(col)<2: continue
+    
+    parm = col[0].strip()
+    val = col[1].strip()
+    if parm=='battery.charge':
+        msg_list.append('Battery level: %s' % val)
+        continue
+
+    if parm=='input.voltage':
+        msg_list.append('Input voltage: %s' % val)
+        continue
+
+    
+
+txtmsg = 'QUBIC is running on battery!\n%s' % '\n'.join(msg_list)
 
 msg = MIMEText(txtmsg)
 msg['Subject']='UPS alert from QUBIC'
