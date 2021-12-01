422c0c6ba343298b22341878038aa2fad53d78a2
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2021-12-01 08:40:11 +0100

    wrapper script to log compressor status

---
+++ scripts/compressor_log.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# $Id: compressor_log.sh
+# $auth: Steve Torchinsky <satorchi@apc.in2p3.fr>
+# $created: Tue 30 Nov 2021 19:40:21 CET
+# $license: GPLv3 or later, see https://www.gnu.org/licenses/gpl-3.0.txt
+#
+#           This is free software: you are free to change and
+#           redistribute it.  There is NO WARRANTY, to the extent
+#           permitted by law.
+#
+# wrapper to log compressor status
+
+HK_DIR=/home/qubic/data/temperature/broadcast
+BIN_DIR=/usr/local/bin
+
+${BIN_DIR}/compressor_commander.py 1 log >> ${HK_DIR}/compressor1_log.txt
+${BIN_DIR}/compressor_commander.py 2 log >> ${HK_DIR}/compressor2_log.txt
+
+#compressor_log.sh
