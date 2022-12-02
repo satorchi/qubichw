2b02414b809e4382954411cc8c25ff39a00de426
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2022-12-02 13:51:39 +0100

    remove unnecessary commented code

--- scripts/ups_alarm.py
+++ scripts/ups_alarm.py
@@ -12,27 +12,8 @@ $license: GPLv3 or later, see https://www.gnu.org/licenses/gpl-3.0.txt
 send a warning that we're on UPS
 '''
 
-# following example at: http://docs.python.org/library/email-examples.html
-#import smtplib
-#from email.mime.text import MIMEText
-#import sys,os,time,subprocess
 from qubichk.ups import get_ups_info
 from qubichk.send_telegram import send_telegram
-
-# cmd = 'hostname'
-# proc = subprocess.Popen(cmd,stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-# out,err = proc.communicate()
-# hostname = out.decode().strip()
-
-
-# msg = MIMEText(txtmsg)
-# msg['Subject']='UPS alert from QUBIC'
-# msg['To']='Steve Torchinsky <satorchi@apc.in2p3.fr>'
-# msg['From']='%s <satorchi@apc.in2p3.fr>' % hostname
-# s = smtplib.SMTP('apcrelay.in2p3.fr')
-# s.sendmail(msg['From'], msg['To'], msg.as_string())
-# s.quit()
-
 
 info = get_ups_info()
 
