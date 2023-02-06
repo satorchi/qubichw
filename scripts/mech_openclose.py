bdb9d0547b55c348f55323d66ecc1d36a599cdd3
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2023-02-06 14:05:19 +0100

    allow command line arguments for mech_openclose

--- scripts/mech_openclose.py
+++ scripts/mech_openclose.py
@@ -17,18 +17,36 @@ import datetime as dt
 
 from qubichk.entropy_hk import entropy_hk
 
-
+datefmt = '%Y-%m-%d %H:%M:%S UT'
 hk = entropy_hk()
 nsteps = 150000 # number of steps to open
 nsqueeze = 500  # extra number of steps to squeeze closed a bit more
-wait_open = 60  # wait time in seconds before closing again
+wait_open = 120  # wait time in seconds before closing again
+
+# parse arguments
+for arg in sys.argv:
+    if arg.find('--nsteps=')>=0:
+        val_str = arg.split('=')[-1].strip()
+        nsteps = eval(val_str)
+        continue
+
+    if arg.find('--squeeze=')>=0:
+        val_str = arg.split('=')[-1].strip()
+        nsqueeze = eval(val_str)
+        continue
+
+    if arg.find('--wait=')>=0:
+        val_str = arg.split('=')[-1].strip()
+        wait_open = eval(val_str)
+        continue
+        
 
 now = dt.datetime.utcnow()
-print('%s | Opening Mechanical Heat Switch' % now.strftime('%Y-%m-%d %H:%M:%S.%f UT'))
+print('%s | Opening Mechanical Heat Switch' % now.strftime(datefmt))
 hk.mech_command(1,nsteps,'open')
 
 time.sleep(wait_open)
 now = dt.datetime.utcnow()            
-print('%s | Closing Mechanical Heat Switch' % now.strftime('%Y-%m-%d %H:%M:%S.%f UT'))
+print('%s | Closing Mechanical Heat Switch' % now.strftime(datefmt))
 hk.mech_command(1,nsteps+nsqueeze,'close')
     
