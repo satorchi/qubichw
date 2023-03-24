dead1a203a3ca010911b64b69d6da6e1ce1c0012
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2023-03-24 11:55:41 +0100

    check for socket refused

--- qubichk/obsmount.py
+++ qubichk/obsmount.py
@@ -92,6 +92,12 @@ class obsmount:
             self.error = 'TIMEOUT'
             print('ERROR: could communicate because of %s to %s:%s' % (self.error,self.mount_ip,port_num))
             return None
+        except: socket.ConnectionRefusedError
+            self.subscribed = False
+            self.error = 'SOCKET CONNECTION REFUSED'
+            print('ERROR: could communicate because of %s to %s:%s' % (self.error,self.mount_ip,port_num))
+            return None
+        
         except:
             self.subscribed = False
             self.error = 'SOCKET ERROR'
