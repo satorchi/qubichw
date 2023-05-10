6e280e8a957e0009671931dda70cfcc09615be3a
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2023-05-10 09:02:03 +0200

    obsmount: read a chunk after subscribing

--- qubichk/obsmount.py
+++ qubichk/obsmount.py
@@ -151,6 +151,8 @@ class obsmount:
             self.printmsg('sending OK')
             ans = self.sock[port].send('OK'.encode())
             self.printmsg('return from socket.send: %s' % ans)
+            ans = self.sock[port].recv(128)
+            self.printmsg('first read chunk from socket: %s' % ans.decode())
             self.subscribed[port] = True
             self.error = None
         except socket.timeout:
