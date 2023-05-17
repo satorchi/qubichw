057fc60785963d7dd9fe370079a759ed9d25aed8
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2023-05-17 21:15:31 +0200

    methods to send/receive usbthermometer data

---
+++ qubichk/usbthermometer_hk.py
@@ -0,0 +1,45 @@
+'''
+$Id: usbthermometer_hk.py
+$auth: Steve Torchinsky <satorchi@apc.in2p3.fr>
+$created: Wed 17 May 2023 21:04:26 CEST
+$license: GPLv3 or later, see https://www.gnu.org/licenses/gpl-3.0.txt
+
+          This is free software: you are free to change and
+          redistribute it.  There is NO WARRANTY, to the extent
+          permitted by law.
+
+receive the temperature data broadcast from the RaspberryPi with the usbthermometer
+It's the temperature of the outside of the cryostat
+'''
+import time,socket
+import datetime as dt
+import numpy as np
+
+class usbthermometer_hk:
+    verbosity = 0
+
+    def __init__(self,port=42377):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+        self.client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.client.bind(('', port))
+        return
+
+    def get_latest(self):
+        '''
+        get the latest data.  loop a few times to eliminate latency
+        '''
+        time_diff = 1e6
+        for idx in range(300):
+            x, addr = client.recvfrom(1024)
+            data_tuple = struct.unpack(fmts,x)
+            tstamp = data_tuple[1]
+            date = dt.datetime.utcfromtimestamp(tstamp)
+            rx_tstamp = time.time()
+            time_diff = np.abs(rx_tstamp - tstamp)
+            val = data_tuple[2]
+            if time_diff<0.005: break
+
+        return (tstamp,val)
+
+
+    
---
+++ qubichw/read_usbthermometer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+'''
+$Id: read_usbthermometer.py
+$auth: Steve Torchinsky <satorchi@apc.in2p3.fr>
+$created: Wed 17 May 2023 18:58:11 CEST
+$license: GPLv3 or later, see https://www.gnu.org/licenses/gpl-3.0.txt
+
+          This is free software: you are free to change and
+          redistribute it.  There is NO WARRANTY, to the extent
+          permitted by law.
+
+read and broadcast the temperature from the usb thermometer
+'''
+from digitemp.master import UART_Adapter
+from digitemp.device import TemperatureSensor
+from digitemp.exceptions import OneWireException
+import time,socket
+import numpy as np
+
+# data is sent as a numpy record, to be unpacked by qubic-central
+rec = np.recarray(names="STX,TIMESTAMP,VALUE",formats="uint8,float64,float32",shape=(1))
+rec[0].STX = 0xAA
+
+IP_QUBIC_CENTRAL = "192.168.2.1"
+PORT = 42377
+rx = (IP_QUBIC_CENTRAL,PORT)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+bus = UART_Adapter('/dev/thermometer')
+roms = bus.get_connected_ROMs()
+rom = roms[0]
+sensor = TemperatureSensor(bus, rom)
+
+while True:
+    T = sensor.get_temperature()
+    Tkelvin = T + 273.15
+    rec[0].TIMESTAMP = np.float64(time.time())
+    rec[0].VALUE = np.float32(Tkelvin)
+    s.sendto(rec,rx)
+    time.sleep(0.2)
+
+
+    
