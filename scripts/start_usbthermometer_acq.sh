9964d1950ea46bf7ede61edbdb1a6449db9f08ce
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2023-05-23 10:49:25 +0200

    bugfix: comments for bash

--- scripts/start_usbthermometer_acq.sh
+++ scripts/start_usbthermometer_acq.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-'''
-$Id: start_usbthermometer_acq.sh
-$auth: Steve Torchinsky <satorchi@apc.in2p3.fr>
-$created: Tue 23 May 2023 07:34:02 CEST
-$license: GPLv3 or later, see https://www.gnu.org/licenses/gpl-3.0.txt
+# $Id: start_usbthermometer_acq.sh
+# $auth: Steve Torchinsky <satorchi@apc.in2p3.fr>
+# $created: Tue 23 May 2023 07:34:02 CEST
+# $license: GPLv3 or later, see https://www.gnu.org/licenses/gpl-3.0.txt
+#
+#           This is free software: you are free to change and
+#           redistribute it.  There is NO WARRANTY, to the extent
+#           permitted by law.
+#
+# start the usbthermometer reading and broadcasting
 
-          This is free software: you are free to change and
-          redistribute it.  There is NO WARRANTY, to the extent
-          permitted by law.
-
-start the usbthermometer reading and broadcasting
-'''
 if ! ps -aux | grep "read_usbthermometer.py" | grep -v -e grep -e SCREEN; then  
 	echo "Usbthermometer acquisition not running";
 	screen -X -S usbthermometer quit
