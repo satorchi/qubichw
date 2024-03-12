3db2916f78cc61bcb4a2865ae664848ec28cd7b2
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2024-03-12 08:29:23 +0100

    bugfix: start weather script for insde weather

--- scripts/start_weather.sh
+++ scripts/start_weather.sh
@@ -15,7 +15,7 @@ if ! ps auxw | grep "/usr/local/bin/weather.py" | grep -v -e grep -e SCREEN -e i
     screen -X -S weather quit
     echo "Starting a new screen and launching the weather logger"
     cd $HOME/data/temperature/broadcast
-    screen -S weather -d -m /usr/local/bin/weather.py --log --period=3
+    screen -S weather -d -m /usr/local/bin/weather.py --log --period=3 --logfile=$HOME/data/temperature/broadcast/weather.txt
 else
     echo "weather logger already running"
 fi
@@ -26,7 +26,7 @@ if ! ps auxw | grep "/usr/local/bin/weather.py" | grep inside_weather | grep -v 
     screen -X -S inside_weather quit
     echo "Starting a new screen and launching the indoor weather logger"
     cd $HOME/data/temperature/broadcast
-    screen -S weather -d -m /usr/local/bin/weather.py --log --period=3 --logfile=/home/qubic/data/temperature/broadcast/inside_weather.txt --server=192.168.88.13
+    screen -S weather -d -m /usr/local/bin/weather.py --log --period=3 --logfile=$HOME/data/temperature/broadcast/inside_weather.txt --server=192.168.88.13
 else
     echo "inside weather logger already running"
 fi
