1612a9362036ebcf67032e8b047092ec3c7f06c7
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2025-02-19 08:37:17 +0100

    remove debugging changes

--- qubichw/read_MCP9808_thermometer.py
+++ qubichw/read_MCP9808_thermometer.py
@@ -73,9 +73,8 @@ def read_temperatures(verbosity=0):
     # Temp MSB, TEMP LSB
     base_address = 0x18
     for idx,Tidx in enumerate(sensors):
-        idx = Tidx
         addr = base_address + Tidx
-        if verbosity>0: print('addr: %i' % addr)
+        if verbosity>1: print('addr: %i' % addr)
 
         try:
             data = bus.read_i2c_block_data(addr, 0x05, 2)
