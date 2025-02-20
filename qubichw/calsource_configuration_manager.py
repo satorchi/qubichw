47b2352c3d12405e20505b4360eb926a620eef46
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2025-02-20 13:03:09 +0100

    bugfix: missing bracket

--- qubichw/calsource_configuration_manager.py
+++ qubichw/calsource_configuration_manager.py
@@ -250,7 +250,7 @@ class calsource_configuration_manager():
         listen for a command string arriving on socket
         this message is called by the "manager"
         '''
-        self.log('listening on %s port %i' % (self.receiver,self.broadcast_port)
+        self.log('listening on %s port %i' % (self.receiver,self.broadcast_port))
 
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
         s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
