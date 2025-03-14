0ceb57f809bf85d2c05d856828041c4d891ac6f5
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2025-03-14 09:53:53 +0100

    start script for heater manager

--- setup.py
+++ setup.py
@@ -162,7 +162,8 @@ scripts = ['scripts/calsource_commander.py',
            'scripts/run_MCP9808_broadcast.py',
            'scripts/start_MCP9808_acquisition.sh',
            'scripts/start_MCP9808_broadcast.sh',
-           'scripts/run_heater_manager.py'
+           'scripts/run_heater_manager.py',
+           'scripts/start_heater_manager.sh'
            ]
 if len(sys.argv)>1 and sys.argv[1]=='install' and exec_dir_ok:
     print('installing executable scripts...')
