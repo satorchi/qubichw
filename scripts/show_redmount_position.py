ab7bf262a35beef4cf093c2654798de741b27d79
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2025-06-05 19:54:51 +0200

    old show_position renamed to show_redmount_position

---
+++ scripts/show_redmount_position.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+'''
+$Id: show_position.py
+$auth: Steve Torchinsky <satorchi@apc.in2p3.fr>
+$created: Fri 18 Feb 2022 13:21:47 CET
+$license: GPLv3 or later, see https://www.gnu.org/licenses/gpl-3.0.txt
+
+          This is free software: you are free to change and
+          redistribute it.  There is NO WARRANTY, to the extent
+          permitted by law.
+
+show the position of the platform
+'''
+import datetime as dt
+from qubichk.platform import get_position
+
+header = '%s   %s   %s' % ('time'.center(20),'az'.center(7),'el'.center(7))
+print(header,end='\n')
+
+while True:
+    az,el,azwarn,elwarn = get_position()
+    date_str = dt.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+    if type(az)==str:
+        az_str = 'ERROR'.center(9)
+    else:
+        if azwarn:
+            az_str = '%7.2f ?' % az
+        else:
+            az_str = '%7.2f  ' % az
+    if type(el)==str:
+        el_str = 'ERROR'.center(9)
+    else:
+        el_str = '%7.2f  ' % el
+    
+    line = '%s   %s   %s' % (date_str,az_str,el_str)
+    print(line,end='\r')
+
+    
