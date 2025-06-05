ec886bb5f3fa097b91b873f4ba15010a77456d37
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2025-06-05 19:56:34 +0200

    new show_position for the observation mount

--- scripts/show_position.py
+++ scripts/show_position.py
@@ -2,37 +2,16 @@
 '''
 $Id: show_position.py
 $auth: Steve Torchinsky <satorchi@apc.in2p3.fr>
-$created: Fri 18 Feb 2022 13:21:47 CET
+$created: Thu 05 Jun 2025 19:55:32 CEST
 $license: GPLv3 or later, see https://www.gnu.org/licenses/gpl-3.0.txt
 
           This is free software: you are free to change and
           redistribute it.  There is NO WARRANTY, to the extent
           permitted by law.
 
-show the position of the platform
+show the current position of the observation mount
 '''
-import datetime as dt
-from qubichk.platform import get_position
+from qubichk.obsmount import obsmount
 
-header = '%s   %s   %s' % ('time'.center(20),'az'.center(7),'el'.center(7))
-print(header,end='\n')
-
-while True:
-    az,el,azwarn,elwarn = get_position()
-    date_str = dt.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-    if type(az)==str:
-        az_str = 'ERROR'.center(9)
-    else:
-        if azwarn:
-            az_str = '%7.2f ?' % az
-        else:
-            az_str = '%7.2f  ' % az
-    if type(el)==str:
-        el_str = 'ERROR'.center(9)
-    else:
-        el_str = '%7.2f  ' % el
-    
-    line = '%s   %s   %s' % (date_str,az_str,el_str)
-    print(line,end='\r')
-
-    
+mount = obsmount()
+mount.show_azel()
