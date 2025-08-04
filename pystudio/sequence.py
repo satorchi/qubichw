4fa49299084688403bc9ce11076d34a38269bc08
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2025-08-04 16:02:52 +0200

    SQUID optimization sequence and improved default settings

--- pystudio/sequence.py
+++ pystudio/sequence.py
@@ -20,22 +20,72 @@ from qubichk.obsmount import obsmount
 # defaults
 default_setting = {}
 default_setting['asicNum'] = [1,2] # both ASICs
-default_setting['Voffset'] = 5.5 # Volts
-default_setting['amplitude'] = 7.0 # Volts
 default_setting['undersampling'] = 1000
 default_setting['increment'] = 1
-default_setting['duration'] = 120 # seconds
+default_setting['Apol'] = 7
+
 default_setting['elmin'] = 50 # minimum permitted elevation
 default_setting['elmax'] = 70 # maximum permitted elevation
-default_setting['azmin'] = 0  # minimum permitted azimuth
-default_setting['azmax'] = 25 # maximum permitted azimuth (changed to 25 on 2025-06-06 15:51:25 UT)
+default_setting['azmin'] = 61  # minimum permitted azimuth
+default_setting['azmax'] = 115 # maximum permitted azimuth (changed to 25 on 2025-06-06 15:51:25 UT)
 default_setting['azstep'] = 5 # default step size for azimuth movement for skydips
+
+default_setting['I-V'] = {}
+default_setting['I-V']['Voffset'] = 5.5
+default_setting['I-V']['amplitude'] = 7.0
+default_setting['I-V']['duration'] = 120
+default_setting['I-V']['Aplitude'] = 180
+default_setting['I-V']['FeedbackRelay'] = 10
+
+default_setting['SQUID'] = {}
+default_setting['SQUID']['Voffset'] = 8
+default_setting['SQUID']['amplitude'] = 1
+default_setting['SQUID']['duration'] = 2
+default_setting['SQUID']['Aplitude'] = 1800
+
+default_setting['observation'] = {}
+default_setting['observation']['Voffset'] = 3.0
+default_setting['observation']['Aplitude'] = 1800
+default_setting['observation']['FeedbackRelay'] = 100
 
 default_setting['ASIC 1'] = {}
 default_setting['ASIC 2'] = {}
 
 default_setting['ASIC 1']['Spol'] = 10
 default_setting['ASIC 2']['Spol'] = 12
+
+def get_default_setting(self,parm=None,asic=None,measurement=None):
+    '''
+    get the default value, for a particular ASIC is requested
+    '''
+
+    if parm in default_setting.keys():
+        return default_setting[parm]
+
+    if asic is None and measurement is None:
+        return None
+
+    if asic is not None:
+        asicKey = 'ASIC %i' % asic
+        
+    if measurement in default_setting.keys():
+        if asic is not None and asicKey in default_setting[measurement].keys():
+            if parm in default_setting[measurement][asicKey].keys():
+                return default_setting[measurement][asicKey][parm]
+        if parm in default_setting[measurement].keys():
+            return default_setting[measurement][parm]
+            
+    if asic is None:
+        return None
+            
+    if asicKey not in default_setting.keys():
+        return None
+
+    if parm not in default_setting[asicKey].keys():
+        return None
+
+    return default_setting[asicKey][parm]
+        
 
 def set_bath_temperature(self,Tbath,timeout=120,precision=0.003):
     '''
@@ -92,12 +142,12 @@ def do_IV_measurement(self,
 
     #####################################
     # defaults
-    if asicNum is None: asicNum = default_setting['asicNum']
-    if Voffset is None: Voffset = default_setting['Voffset']
-    if amplitude is None: amplitude = default_setting['amplitude']
-    if undersampling is None: undersampling = default_setting['undersampling']
-    if increment is None: increment = default_setting['increment']
-    if duration is None: duration = default_setting['duration']
+    if asicNum is None: asicNum = self.get_default_setting('asicNum')
+    if Voffset is None: Voffset = self.get_default_setting('Voffset',measurement='I-V')
+    if amplitude is None: amplitude = self.get_default_setting('amplitude',measurement='I-V')
+    if undersampling is None: undersampling = self.get_default_setting('undersampling',measurement='I-V')
+    if increment is None: increment = self.get_default_setting('increment',measurement='I-V')
+    if duration is None: duration = self.get_default_setting('duration',measurement='I-V')
     if comment is None: comment = 'I-V measurement sent by pystudio'
 
 
@@ -195,6 +245,104 @@ def do_NEP_measurement(self,
 
     print('%s - NEP measurement completed' % utcnow().strftime('%Y-%m-%d %H:%M:%S'))
     return
+
+
+def do_SQUID_optimization(self,
+                          asicNum=None,
+                          Voffset=None,
+                          amplitude=None,
+                          undersampling=None,
+                          increment=None,
+                          Tbath=None,
+                          duration=None,
+                          aplitude=None,
+                          Apol=None,
+                          comment=None):
+    '''
+    run the sequence to measure the SQUID optimum bias
+    '''
+
+    #####################################
+    # defaults
+    if asicNum is None: asicNum = self.get_default_setting('asicNum')
+    if Voffset is None: Voffset = self.get_default_setting('Voffset',measurement='SQUID')
+    if amplitude is None: amplitude = self.get_default_setting('amplitude',measurement='SQUID')
+    if undersampling is None: undersampling = self.get_default_setting('undersampling',measurement='SQUID')
+    if increment is None: increment = self.get_default_setting('increment',measurement='SQUID')
+    if duration is None: duration = self.get_default_setting('duration',measurement='SQUID')
+    if aplitude is None: aplitude = self.get_default_setting('Aplitude',measurement='SQUID')
+    if Apol is None: Apol = self.get_default_setting('Apol',measurement='SQUID')
+    if comment is None: comment = 'SQUID optimization measurement sent by pystudio'
+    if Tbath is None: Tbath = 0.420
+
+    #####################################
+    # make sure the bias does not go out of acceptable range
+    Vmax = Voffset + 0.5*amplitude
+    if (Vmax>9):
+        print('STOP:  Maximum bias voltage will be greater than 9 Volts: %.2f V' % Vmax)
+        return None
+
+    Vmin = Voffset - 0.5*amplitude
+    if (Vmin<1.0):
+        print('STOP:  Minimum bias voltage will be less than 1 Volt: %.2f V' % Vmin)
+        return None
+
+    #####################################
+    # check for desired bath temperature
+    Tbath_ok = self.set_bath_temperature(Tbath)
+
+    if not Tbath_ok:
+        print("Tbath temperature not reached.  I'm not continuing with the SQUID optimization measurement.")
+        return None
+
+
+    #####################################
+    # configure the bolometers
+
+    # stop all regulations
+    ack = self.send_stopFLL(asicNum)
+
+    # set the Aplitude (Amplitude Modulation)
+    ack = self.send_Aplitude(asicNum,aplitude)
+
+    # set the SQUID amplitude (Apol)
+    ack = self.send_Apol(asicNum,Apol)
+
+    # configure sine curve bias
+    ack = self.send_TESDAC_SINUS(asicNum,amplitude,Voffset,undersampling,increment)
+
+    # get current temperature
+    mgc = iMACRT(device='mgc')
+    Tmeas = mgc.get_mgc_measurement()
+    if Tmeas=='':
+        T_str = ''
+    else:
+        T_str = '%.0fmK' % (1000*Tmeas)
+
+    
+    # loop through the Spol values
+    for bias_index in range(16):
+        # set the Spol value
+        ack = self.send_Spol(asicNum,bias_index)
+        time.sleep(0.1)
+        
+        dataset_name = 'SQUIDs_opt_bias_aplitude_%i_%i' % (aplitude,bias_index)
+        ack = self.send_startAcquisition(dataset_name,comment)
+
+        # wait for measurement
+        time.sleep(duration)
+
+        # stop the acquisition
+        ack = self.send_stopAcquisition()
+
+
+    # switch off the temperature feedback
+    mgc.set_mgc_pid(0)
+    mgc.disconnect()
+    
+    print('%s - SQUID optimization measurement completed' % utcnow().strftime('%Y-%m-%d %H:%M:%S'))
+    return
+
 
 def start_observation(self,Voffset=None,Tbath=None,title=None,comment=None):
     '''
