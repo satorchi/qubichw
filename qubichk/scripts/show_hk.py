e8fe84e2b38e27bf9202fddebe0d697846392534
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2025-12-03 15:40:56 +0100

    battery status is given by key: battery.charge

--- qubichk/scripts/show_hk.py
+++ qubichk/scripts/show_hk.py
@@ -224,7 +224,7 @@ def read_ups():
     lines.append(line)
 
     
-    label = 'battery status'
+    label = 'battery charge'
     tstamps.append(tstamp)
     if vals[2].find(label.replace(' ','.'))<0:
         val_str = 'NO UPS INFO'
