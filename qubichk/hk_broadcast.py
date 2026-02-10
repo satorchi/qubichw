898d1b47a4094ae394c8521492ee4dbebe140e36
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2026-02-10 08:17:15 +0100

    disconnect from obsmount server to avoid conflict with science data acquisition of pointing data

--- qubichk/hk_broadcast.py
+++ qubichk/hk_broadcast.py
@@ -349,6 +349,8 @@ class hk_broadcast :
             self.record[recname][0] = val
             self.log_hk(recname,tstamp,val,tstamp_rx)
 
+        # disconnect to free up access to pointing data
+        self.hk_azel.disconnect()
         return self.record
 
     def get_cryostat_temperature_hk(self):
