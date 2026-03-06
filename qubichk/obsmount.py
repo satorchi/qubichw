f2fb213f34ffeaa658b54a6a7b9da51d59ba499c
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2026-03-06 19:42:06 +0100

    increase pause before trying to read the command echo

--- qubichk/obsmount.py
+++ qubichk/obsmount.py
@@ -319,7 +319,7 @@ class obsmount:
             return self.return_with_error(retval)
 
         # try to get the command echo
-        time.sleep(0.1)
+        time.sleep(0.25)
         try:
             cmd_echo = self.sock[port].recv(1024)
         except:
