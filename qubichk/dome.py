df360f312164a6f0fe2672ad38f9895b404adb7a
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2026-07-22 14:50:22 +0200

    make_errmsg imported from satorchipy

--- qubichk/dome.py
+++ qubichk/dome.py
@@ -11,7 +11,8 @@ $license: GPLv3 or later, see https://www.gnu.org/licenses/gpl-3.0.txt
 get the dome status
 '''
 from urllib.request import urlopen
-from qubichk.utilities import get_known_hosts, make_errmsg
+from qubichk.utilities import get_known_hosts
+from satorchipy.utilities import make_errmsg
 known_hosts = get_known_hosts()
 dome_server = known_hosts['dome']
 value_names = {0:'Manual B',
